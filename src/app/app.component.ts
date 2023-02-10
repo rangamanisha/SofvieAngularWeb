7aafc50745fc56c92f9e014ae35972bbba0ec0bb
Research <rangamanisha11@gmail.com>
2023-02-09 19:18:25 -0500

    created checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'SofvieApp';
+  _equipmentlist:equipment[];
+  // _student:student;
+  // _studentlist:student[]=[];
+  uniquekey:number=0;
+
+
+  ngOnInit()
+  {
+    this.getEquipment();
+    // this._student= new student();
+  }
+
+  getEquipment()
+  {
+    this._equipmentlist=[
+      {id:1, name:"record1", isselected:false},
+      {id:2, name:"record2", isselected:false},
+      {id:3, name:"record3", isselected:false},
+      {id:4, name:"record4", isselected:false},
+      {id:5, name:"record5", isselected:false},
+      {id:6, name:"record6", isselected:false}
+
+    ]
+  }
+
+  onchange()
+  {
+    console.log(this._equipmentlist);
+  }
+}
+class equipment{
+  id: number;
+  name: string;
+  isselected: boolean;
 }
